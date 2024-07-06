42d192c4b5ba380555057a379227bf0b13346628
Gegvin <Gegaru123@mail.ru>
2024-07-06 21:17:43 +0300

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -8,13 +8,13 @@ def create_virtual_env():
 
 def install_requirements():
     print("Устанавливаются зависимости...")
-    # На Windows и Unix пути к pip разные, определим это:
+    # На Windows и Unix пути к pip разные, определяем:
     pip_executable = os.path.join('myenv', 'Scripts', 'pip') if os.name == 'nt' else os.path.join('myenv', 'bin', 'pip')
     subprocess.run([pip_executable, 'install', '-r', 'requirements.txt'])
 
 def run_application():
     print("Запускается приложение...")
-    # На Windows и Unix пути к python разные, определим это:
+    # На Windows и Unix пути к python разные, определяем:
     python_executable = os.path.join('myenv', 'Scripts', 'python') if os.name == 'nt' else os.path.join('myenv', 'bin', 'python')
     subprocess.Popen([python_executable, 'app.py'])
 
