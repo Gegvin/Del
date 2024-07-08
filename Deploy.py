e5dd0b1d7bbe71b853f2f38243f77fbeaf0805e8
Gegvin <Gegaru123@mail.ru>
2024-07-08 18:44:10 +0300

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -16,12 +16,15 @@ def run_application():
     print("Запускается приложение...")
     # На Windows и Unix пути к python разные, определяем:
     python_executable = os.path.join('myenv', 'Scripts', 'python') if os.name == 'nt' else os.path.join('myenv', 'bin', 'python')
-    subprocess.Popen([python_executable, 'app.py'])
+    subprocess.Popen([python_executable, 'main.py'])
 
 def main():
-    create_virtual_env()
-    install_requirements()
-    run_application()
+    if os.path.exists('myenv'):
+        run_application()
+    else:
+        create_virtual_env()
+        install_requirements()
+        run_application()
 
 if __name__ == '__main__':
     main()
